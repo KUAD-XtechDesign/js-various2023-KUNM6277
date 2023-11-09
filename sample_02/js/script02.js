ba1883d90a12f8131dca5fc51b26e5d0c59564ea
H! <138648396+KUNM6277@users.noreply.github.com>
2023-11-09 21:39:03 +0900

    01

--- sample_02/js/script02.js
+++ sample_02/js/script02.js
@@ -6,66 +6,46 @@
   let separate
   let scrollRatio
 
- // 当页面滚动、窗口大小改变或页面加载完成时执行以下代码
-$(window).on("scroll resize load", function () {
-  // 获取窗口高、度整个文档的高度和宽度
-  windowH = $(this).height();
-  documentH = $(document).height();
-  documentW = $(document).width();
 
-  // 获取页面垂直滚动位置（滚动条位置）
-  scrollTop = $(this).scrollTop();
 
-  // 计算滚动的比例，范围从0到1
-  scrollRatio = scrollTop / (documentH - windowH);
 
-  // 将文档高度分成5等分，用于后续的条件判断
-  separate = documentH / 5;
+// 获取包含所有图片的父容器
+var imageContainer = document.querySelector('.set1');
 
-  // 根据滚动位置，为页面的 body 元素添加不同的 CSS 类
-  if (scrollTop < separate) {
-    $("body").removeClass().addClass("one");
-  } else if (scrollTop < separate * 2) {
-    $("body").removeClass().addClass("two");
-  } else if (scrollTop < separate * 3) {
-    $("body").removeClass().addClass("three");
-  } else if (scrollTop < separate * 4) {
-    $("body").removeClass().addClass("four");
-  } else {
-    $("body").removeClass().addClass("five");
+// 鼠标悬停监听
+imageContainer.addEventListener('mouseover', function (event) {
+  // 检查是否是图片元素
+  if (event.target.classList.contains('hoverable-image')) {
+    // 效果
+    event.target.style.transform = 'scale(1.2)';
+    event.target.style.filter = 'brightness(80%)';
+
+    // 显示小窗口
+    var hoverWindow = event.target.nextElementSibling;
+    if (hoverWindow && hoverWindow.classList.contains('hover-window')) {
+      hoverWindow.style.display = 'block';
+    }
   }
+});
 
-  // 输出窗口高度、文档高度和滚动位置到控制台，用于调试
-  console.log(windowH, documentH, scrollTop);
+// 添加鼠标离开事件监听器
+imageContainer.addEventListener('mouseout', function (event) {
+  // 检查是否是图片元素
+  if (event.target.classList.contains('hoverable-image')) {
+    // 恢复原始效果
+    event.target.style.transform = 'scale(1)';
+    event.target.style.filter = 'brightness(100%)';
 
-
-  $("#wave").css("background-position-x", -scrollTop / 10);
-
+    // 隐藏小窗口
+    var hoverWindow = event.target.nextElementSibling;
+    if (hoverWindow && hoverWindow.classList.contains('hover-window')) {
+      hoverWindow.style.display = 'none';
+    }
+  }
 });
 
 
-
-  //Menuボタンを押した時
-  $("#btn01").on("click",function(){
-    $("html, body").animate({scrollTop:0},600, "swing");
-  })
-
-  $("#btn02").on("click",function(){
-    $("html, body").animate({scrollTop:separate*1},600, "swing");
-  })
-
-  $("#btn03").on("click",function(){
-    $("html, body").animate({scrollTop:separate*2},600, "swing");
-  })
-
-  $("#btn04").on("click",function(){
-    $("html, body").animate({scrollTop:separate*3},600, "swing");
-  })
-
-  $("#btn05").on("click",function(){
-    $("html, body").animate({scrollTop:separate*4},600, "swing");
-  })
-
+//小窗口结束
 
   // 监听滚动事件
 $(window).on("scroll", function () {
